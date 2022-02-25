8a75efc9a882d0f1c4146cb01612497b73bb9af4
anketmanoj <anket.m@live.com>
2022-02-25 18:22:26 +0400

    certificate model created

--- certificates/models.py
+++ certificates/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Certificates(models.Model):
+    """
+    Model for certificates
+    """
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
